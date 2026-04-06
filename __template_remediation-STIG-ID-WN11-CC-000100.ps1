7e3ed2b715e235698b07d383ba333d47ea787b41
Oliver Ponder <80069923+oponder2000@users.noreply.github.com>
2026-04-06 16:58:46 -0500

    Update __template_remediation-STIG-ID-WN11-CC-000100.ps1

--- __template_remediation-STIG-ID-WN11-CC-000100.ps1
+++ __template_remediation-STIG-ID-WN11-CC-000100.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+    This PowerShell script prevents downloading print driver packages over HTTP.
 
 .NOTES
     Author          : Oliver Ponder
