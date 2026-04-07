982f97dcd5c68facac1ce1da4909743614747192
Oliver Ponder <80069923+oponder2000@users.noreply.github.com>
2026-04-07 13:58:59 -0500

    Update and rename __template_remediation-STIG-ID-WN11-CC-000205.ps1 to remediation-STIG-ID-WN11-CC-000205.ps1

--- remediation-STIG-ID-WN11-CC-000205.ps1
+++ remediation-STIG-ID-WN11-CC-000205.ps1
@@ -22,7 +22,7 @@
 .USAGE
     Put any usage instructions here.
     Example syntax: 
-    PS C:\> .\__remediation_template(STIG-ID-WN11-CC-000205).ps1 
+    PS C:\> .\remediation-STIG-ID-WN11-CC-000205.ps1
 #>
 
 
